6537639aa72b348a93751afad46953108e98693e
manishh0360 <manishh0360@gmail.com>
2024-05-10 01:48:49 +0530

    prime number checker done

--- prime.py
+++ prime.py
@@ -1,9 +1,11 @@
 num = int(input("Enter any number: "))
 count = 0
 for i in range(2,num):
-    if (num%i == 0):
+   if (num%i == 0):
         count += 1
-if count == 0:
+if(num==1):
+    print("not prime")
+elif count == 0:
     print("prime")
 else:
     print("not prime")
