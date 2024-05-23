9b977a20594d91a68bf178cf6cfab6fa885171c7
manishh0360 <manishh0360@gmail.com>
2024-05-23 14:16:04 +0530

    time complexity check

---
+++ Greet.py
@@ -0,0 +1,7 @@
+import time
+timestamp= (time.strftime('%H:%M:%S'))
+tt=int(input("Whats the time: "))
+if (tt== 6):
+    print("good morning")
+else:
+    print("nothing")
---
+++ rough.py
@@ -0,0 +1,5 @@
+##
+sum = 0
+for i in range(100000000):
+    sum+=i
+print(sum)
